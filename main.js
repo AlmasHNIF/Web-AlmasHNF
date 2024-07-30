127179769685de4906fe52a33516f89cf25f03ec
AlmasHNIF <156587320+AlmasHNIF@users.noreply.github.com>
2024-07-30 14:05:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+// const el = $("p")
+// const el1 =  document.getElementsByTagName("p")
+// const el2 = document.querySelectorAll("p")
+// const el = $("#hello")
+// const el = $(".world")
+
+// console.log(el)
+// console.log(el1)
+// console.log(el2)
+
+// .html()
+// const el = $("#text")
+// el.html("Hello World")
+
+//.attr()
+// const inp = $("input")
+// inp.attr("type", "checkbox")
+
+// add & remove class
+// el.addClass("red")
+// el.addClass("blue")
+// el.removeClass()
+
+//css
+// inp.css({
+//     border: "1px solid red"
+// })
+
+//cara lama js
+// const el = document.getElementById("text")
+// el.addEventListener("mouseenter", function() {
+//     el.style.border = "1pxsolid"    
+// })
+
+//cara baru
+// const.el = $("#text")
+// el.mouseenter(function() {
+//     el.css("border", "1px solid")
+// })
+
+//atau
+// const inp= $("input")
+// inp.focus(function() {
+//     inp.css("border", "1px solid")
+// })
+
+// const btnShow = $("#show")
+// const btnHide = $("#hide")
+// const btnFadeIn = $("#fadeIn")
+// const btnFadeOut = $("#fadeOut")
+// const btnToogle = $("#toogle")
+
+// const div = $("div")
+
+// btnShow.click(function() {
+// div.show("slow")
+// })
+
+// btnHide.click(function() {
+//     div.hide("slow")
+//     })
+
+// btnFadeIn.click(function() {
+//     div.fadeIn("slow")
+//     })
+
+//  btnFadeOut.click(function() {
+//     div.fadeOut("slow")
+//     })
+
+// btnToogle.click(function(){
+//     div.slideToggle()
+// })
+
+const btnShow= $("#show")
+const btnHide= $("#hide")
+
+
+btnShow.click(function(){
+    div.show("slow")
+})
+
+
